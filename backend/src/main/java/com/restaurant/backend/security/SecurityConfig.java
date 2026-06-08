5a720efed341cded64abe8a45f4786e6cc2af501
GiaHuy2k6 <huysasuke199@gmail.com>
2026-06-08 15:34:48 +0700

    Update SecurityConfig.java

--- backend/src/main/java/com/restaurant/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/restaurant/backend/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.restaurant.backend.security;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
